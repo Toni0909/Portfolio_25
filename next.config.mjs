d8a3dad1f275a9297b2c7bb0fda69a5e1358f7d0
Toni0909 <toni.cvetanoski10@mail.com>
2024-10-16 13:34:42 +0200

    Fehlerbehebung: Import von globe.json und Berechtigungen aktualisiert

--- next.config.mjs
+++ next.config.mjs
@@ -1,43 +1,28 @@
 import { withSentryConfig } from '@sentry/nextjs';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+// Um den Pfad korrekt zu erhalten:
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    // Set up alias for '@' to point to the project root
-    config.resolve.alias['@'] = path.resolve();
+  webpack(config) {
+    // Beispiel: Hier kannst du weiterhin __dirname verwenden
+    config.resolve.alias['@data'] = path.join(__dirname, 'data');
     return config;
   },
 };
 
 export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
-  // Suppresses source map uploading logs during build
   silent: true,
   org: "the-cube-enterprise",
   project: "javascript-nextjs",
 }, {
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Transpiles SDK to be compatible with IE11 (increases bundle size)
   transpileClientSDK: true,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
   hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors.
   automaticVercelMonitors: true,
 });
