3eb19ad787e5304932875a5a8a0c5b9651618769
Toni0909 <toni.cvetanoski10@mail.com>
2024-10-18 17:31:35 +0200

    Error1

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { withSentryConfig } from '@sentry/nextjs';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import nextConfig from 'next/config';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    // your custom webpack config here
+    config.resolve.alias['@'] = path.resolve(__dirname); // Add this line
     return config;
   },
 };
