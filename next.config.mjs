3fa4303a58345e3fce6022fa557f4eadaec0adef
Toni0909 <toni.cvetanoski10@mail.com>
2024-10-18 17:39:58 +0200

    Error13

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 import path from 'path'; // Make sure you have this import
 import { withSentryConfig } from '@sentry/nextjs';
+import { fileURLToPath } from 'url';
+
+// Import metadata for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
